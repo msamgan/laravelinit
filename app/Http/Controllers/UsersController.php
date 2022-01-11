46e896a6fa10ad6c9a12e901c6a69dc122393b90
msamgan <mohdsamgankhan@gmail.com>
2022-01-12 01:48:54 +0400

    route discovery

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class UsersController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
