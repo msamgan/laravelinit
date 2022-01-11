d1c77d7995280bbe3f5bf651728acad408b9d86f
msamgan <mohdsamgankhan@gmail.com>
2022-01-12 01:33:57 +0400

    additional packagaes

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasSlug;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +43,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
--- helpers/methods.php
+++ helpers/methods.php
@@ -2,12 +2,12 @@
 
 if (!function_exists('successResponse')) {
     /**
-     * @param $message
+     * @param string $message
      * @param null $payload
      * @param null $extraData
      * @return array
      */
-    function successResponse($message, $payload = null, $extraData = null): array
+    function successResponse(string $message, $payload = null, $extraData = null): array
     {
 
         $response = [
@@ -24,5 +24,70 @@ if (!function_exists('successResponse')) {
         }
 
         return $response;
+    }
+}
+
+if (!function_exists('errorResponse')) {
+    /**
+     * @param string $message
+     * @param null $errors
+     * @return array
+     */
+    function errorResponse(string $message, $errors = null): array
+    {
+        $response = [
+            'status' => false,
+            'message' => $message,
+        ];
+
+        if ($errors) {
+            $response['errors'] = $errors;
+        }
+
+        return $response;
+    }
+}
+
+if (!function_exists('emptyRequest')) {
+    /**
+     * emptyRequest, remove all the params in request (body and query string)
+     */
+    function emptyRequest()
+    {
+        foreach (request()->all() as $key => $param) {
+            request()->offsetUnset($key);
+        }
+    }
+}
+
+if (!function_exists('dateFormatter')) {
+    /**
+     * @param $date
+     * @return false|string
+     */
+    function dateFormatter($date): string
+    {
+        return date('Y-m-d', strtotime($date));
+    }
+}
+
+if (!function_exists('generatePin')) {
+    /**
+     * generatePin, generates the pin number for a specific provided digit count.
+     * Default is 4 digit
+     *
+     * @param integer $digits
+     * @return integer
+     */
+    function generatePin(int $digits = 4): int
+    {
+        $i = 0;
+        $pin = null;
+        while ($i < $digits) {
+            $pin .= mt_rand(1, 9);
+            $i++;
+        }
+
+        return $pin;
     }
 }
